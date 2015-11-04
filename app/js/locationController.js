95bae789395acb066e29cc800380b8dd29c2a6c9
Michael Roger <bodgeuk12@gmail.com>
2015-11-04 13:00:59 +0000

    Can get current user loactions

--- app/js/locationController.js
+++ app/js/locationController.js
@@ -6,34 +6,36 @@ checkpointApp.controller("LocationController", function($scope, $firebaseObject,
   var syncObject = $firebaseObject(ref);
   syncObject.$bindTo($scope, 'data')
 
-  var geoFire = new GeoFire(ref);
+  var geoFire = new GeoFire(ref.child('current_locations'));
 
-    var getLocation = function() {
-      if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
-        navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
-      } else {
-        alert("Your browser does not support the HTML5 Geolocation API");
-      }
-    };
+  var getLocation = function() {
+    if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
+      navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
+    } else {
+      alert("Your browser does not support the HTML5 Geolocation API");
+    }
+  };
 
-    var geolocationCallback = function(location) {
-      self.latitude = location.coords.latitude;
-      self.longitude = location.coords.longitude;
-    };
+  var geolocationCallback = function(location) {
+    self.latitude = location.coords.latitude;
+    self.longitude = location.coords.longitude;
+    geoFire.set("Michael", [self.latitude, self.longitude])
+    console.log(self.longitude)
+  };
 
-    getLocation();
+  getLocation();
 
-    var errorHandler = function(error) {
-      if (error.code == 1) {
-        alert("Error: PERMISSION_DENIED: User denied access to their location");
-      } else if (error.code === 2) {
-        alert("Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached");
-      } else if (error.code === 3) {
-        alert("Error: TIMEOUT: Calculating the user's location too took long");
-      } else {
-        alert("Unexpected error code")
-      }
-    };
+  var errorHandler = function(error) {
+    if (error.code == 1) {
+      alert("Error: PERMISSION_DENIED: User denied access to their location");
+    } else if (error.code === 2) {
+      alert("Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached");
+    } else if (error.code === 3) {
+      alert("Error: TIMEOUT: Calculating the user's location too took long");
+    } else {
+      alert("Unexpected error code")
+    }
+  };
 
 
   // this.getDistance = function(){
