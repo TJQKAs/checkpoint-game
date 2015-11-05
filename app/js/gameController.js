1da7025f3709826a267a7c3770fd2f2a32848ca3
Michael Roger <bodgeuk12@gmail.com>
2015-11-05 14:00:50 +0000

    Checkpoints can be specific to users

--- app/js/gameController.js
+++ app/js/gameController.js
@@ -3,17 +3,19 @@ checkpointApp.controller('GameController', function(DatabaseDataFactory){
   var self = this;
   var ref = DatabaseDataFactory;
   self.authData = ref.getAuth();
-  var userLink = 'users/' + self.authData.uid
+
 
   // if (self.authData) {
   //   var userId = self.authData.uid
 
+  if (self.authData) {
+
+    var userLink = 'users/' + self.authData.uid
 
     self.startGame = function() {
       console.log(self.checkpoints)
       console.log(self.nextCheckpoint)
       ref.child(userLink).update({checkpoints: self.checkpoints});
-
     };
 
     ref.on('value', function(dataSnapshot){
@@ -22,14 +24,20 @@ checkpointApp.controller('GameController', function(DatabaseDataFactory){
         self.checkpoints = snapshot.val();
       })
 
-      for (var key in self.checkpoints) {
-        var val = self.checkpoints[key];
+      ref.child(userLink).child('checkpoints').once('value', function(snapshot) {
+        self.userCheckpoints = snapshot.val();
+      })
+
+      for (var key in self.userCheckpoints) {
+        var val = self.userCheckpoints[key];
         if (val.located === false) {
-          self.nextCheckpoint = val
+          self.nextUserCheckpoint = val
           break
         }
       }
 
     });
+
+  }
 
 });
--- app/js/locationController.js
+++ app/js/locationController.js
@@ -4,78 +4,87 @@ checkpointApp.controller("LocationController", function($scope, $firebaseObject,
   // var ref = new Firebase("https://resplendent-torch-5391.firebaseio.com/");
   var ref = DatabaseDataFactory;
   var syncObject = $firebaseObject(ref);
+  self.authData = ref.getAuth();
+
   syncObject.$bindTo($scope, 'data');
 
-  var geoFire = new GeoFire(ref);
+  if (self.authData) {
 
-  var getLocation = function() {
-    if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
-      navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
-    } else {
-      alert("Your browser does not support the HTML5 Geolocation API");
-    }
-  };
+    self.authData = ref.getAuth();
+    var userLink = 'users/' + self.authData.uid
 
-  var geolocationCallback = function(location) {
-    self.latitude = location.coords.latitude;
-    self.longitude = location.coords.longitude;
-    // geoFire.set("Michael", [self.latitude, self.longitude]);
-    // console.log(self.longitude)
-  };
+    var geoFire = new GeoFire(ref);
 
-  getLocation();
-
-  var errorHandler = function(error) {
-    if (error.code == 1) {
-      alert("Error: PERMISSION_DENIED: User denied access to their location");
-    } else if (error.code === 2) {
-      alert("Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached");
-    } else if (error.code === 3) {
-      alert("Error: TIMEOUT: Calculating the user's location too took long");
-    } else {
-      alert("Unexpected error code")
-    }
-  };
-
-  this.checkIn = function(checkpoint) {
-    getLocation();
-    console.log(checkpoint)
-    var userLocation = [self.latitude, self.longitude];
-    var targetLocation = [checkpoint.position.latitude, checkpoint.position.longitude];
-    var distanceToTarget = GeoFire.distance(userLocation, targetLocation);
-    var checkpointId = checkpoint.id;
-    var checkpointData = ref.child('checkpoints').child(checkpointId);
-    if (distanceToTarget < 0.02) {
-      checkpointData.update({
-      located: true
-      })
+    var getLocation = function() {
+      if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
+        navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
+      } else {
+        alert("Your browser does not support the HTML5 Geolocation API");
+      }
     };
-    checkpointData.update( changeColour(distanceToTarget) );
-  };
 
-  var changeColour = function(distanceToTarget) {
-    // console.log("Distance to target..." + distanceToTarget);
-    if (distanceToTarget > 5) {
-      return ({color: '#0022FF'})
-    }
-    else if ( distanceToTarget > 3 ) {
-      return ({color: '#00FFF2'})
-    }
-    else if ( distanceToTarget > 1 ) {
-      return ({color: '#00FF26'})
-    }
-    else if ( distanceToTarget > 0.5 ) {
-      return ({color: '#7BFF00'})
-    }
-    else if ( distanceToTarget > 0.2 ) {
-      return ({color: '#FFD900'})
-    }
-    else if ( distanceToTarget > 0.1 ) {
-      return ({color: '#FF5900'})
-    }
-    else {
-      return ({color: '#FF0000'})
-    }
-  };
+    var geolocationCallback = function(location) {
+      self.latitude = location.coords.latitude;
+      self.longitude = location.coords.longitude;
+      // geoFire.set("Michael", [self.latitude, self.longitude]);
+      // console.log(self.longitude)
+    };
+
+    getLocation();
+
+    var errorHandler = function(error) {
+      if (error.code == 1) {
+        alert("Error: PERMISSION_DENIED: User denied access to their location");
+      } else if (error.code === 2) {
+        alert("Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached");
+      } else if (error.code === 3) {
+        alert("Error: TIMEOUT: Calculating the user's location too took long");
+      } else {
+        alert("Unexpected error code")
+      }
+    };
+
+    this.checkIn = function(checkpoint) {
+      getLocation();
+      console.log(checkpoint)
+      var userLocation = [self.latitude, self.longitude];
+      var targetLocation = [checkpoint.position.latitude, checkpoint.position.longitude];
+      var distanceToTarget = GeoFire.distance(userLocation, targetLocation);
+      var checkpointId = checkpoint.id;
+      var checkpointData = ref.child(userLink).child('checkpoints').child(checkpointId);
+      if (distanceToTarget < 0.02) {
+        checkpointData.update({
+        located: true
+        })
+      };
+      checkpointData.update( changeColour(distanceToTarget) );
+    };
+
+    var changeColour = function(distanceToTarget) {
+      // console.log("Distance to target..." + distanceToTarget);
+      if (distanceToTarget > 5) {
+        return ({color: '#0022FF'})
+      }
+      else if ( distanceToTarget > 3 ) {
+        return ({color: '#00FFF2'})
+      }
+      else if ( distanceToTarget > 1 ) {
+        return ({color: '#00FF26'})
+      }
+      else if ( distanceToTarget > 0.5 ) {
+        return ({color: '#7BFF00'})
+      }
+      else if ( distanceToTarget > 0.2 ) {
+        return ({color: '#FFD900'})
+      }
+      else if ( distanceToTarget > 0.1 ) {
+        return ({color: '#FF5900'})
+      }
+      else {
+        return ({color: '#FF0000'})
+      }
+    };
+
+  }
 
 });
