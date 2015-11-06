d9c46fb03a0ede5644e4a3b5aea60b2d88ee2f10
Tim Harrison <mail@trwh.co.uk>
2015-11-06 11:39:44 +0000

    Returning old checkpoints.

--- app/js/app.js
+++ app/js/app.js
@@ -15,51 +15,41 @@ ref.set({
   b: {
       id: 'b',
       name: "Checkpoint Two",
-      realName: "Makers Academy Two",
-      position: { latitude: 51.517399, longitude: -0.07346970000000001 },
+      realName: "Aldgate East Tube Station",
+      position: { latitude: 51.5152, longitude: -0.0722 },
       located: false,
       color: '#FFFFFF',
-      mapPositionTop: 45,
-      mapPositionLeft: 75
+      mapPositionTop: 70,
+      mapPositionLeft: 33
+      },
+   c: {
+      id: 'c',
+      name: "Checkpoint Three",
+      realName: "The Culpepepeper",
+      position: { latitude: 51.516908, longitude: -0.073123 },
+      located: false,
+      color: '#FFFFFF',
+      mapPositionTop: 50,
+      mapPositionLeft: 45
+      },
+    d: {
+      id: 'd',
+      name: "Checkpoint Four",
+      realName: "The Pride of Spitalfields",
+      position: { latitude: 51.518854, longitude: -0.071221 },
+      located: false,
+      color: '#FFFFFF',
+      mapPositionTop: 12,
+      mapPositionLeft: 65
+      },
+    e: {
+      id: 'e',
+      name: "Checkpoint Five",
+      realName: "The White Hart",
+      position: { latitude: 51.520052, longitude: -0.056208 },
+      located: false,
+      color: '#FFFFFF',
+      mapPositionTop: 85,
+      mapPositionLeft: 80
       }
-  // b: {
-  //     id: 'b',
-  //     name: "Checkpoint Two",
-  //     realName: "Aldgate East Tube Station",
-  //     position: { latitude: 51.5152, longitude: -0.0722 },
-  //     located: false,
-  //     color: '#FFFFFF',
-  //     mapPositionTop: 70,
-  //     mapPositionLeft: 33
-  //   },
-   // c: {
-   //    id: 'c',
-   //    name: "Checkpoint Three",
-   //    realName: "The Culpepepeper",
-   //    position: { latitude: 51.516908, longitude: -0.073123 },
-   //    located: false,
-   //    color: '#FFFFFF',
-   //    mapPositionTop: 50,
-   //    mapPositionLeft: 45
-   //  },
-   //  d: {
-   //    id: 'd',
-   //    name: "Checkpoint Four",
-   //    realName: "The Pride of Spitalfields",
-   //    position: { latitude: 51.518854, longitude: -0.071221 },
-   //    located: false,
-   //    color: '#FFFFFF',
-   //    mapPositionTop: 12,
-   //    mapPositionLeft: 65
-   //  },
-   //  e: {
-   //    id: 'e',
-   //    name: "Checkpoint Five",
-   //    realName: "The White Hart",
-   //    position: { latitude: 51.520052, longitude: -0.056208 },
-   //    located: false,
-   //    color: '#FFFFFF',
-   //    mapPositionTop: 85,
-   //    mapPositionLeft: 80
-   //  }
   });
