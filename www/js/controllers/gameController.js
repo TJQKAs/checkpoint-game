5a99bd5747ac461749c5b5e9d142aa7ac46d11e3
Michael Roger <bodgeuk12@gmail.com>
2015-11-11 13:17:38 +0000

    Undone change to warmer/colder

--- www/js/controllers/gameController.js
+++ www/js/controllers/gameController.js
@@ -118,18 +118,14 @@ checkpointApp.controller('GameCtrl', function(DatabaseDataFactory, CurrentLocati
       console.log("HIYA");
 
       userData.once('value', function(snapshot) {
-        if (!$scope.hotterColder) {
-          $scope.hotterColder = 'Awaiting Geolocation...';
+        if ( snapshot.val().distance < $scope.distanceToTarget ) {
+          $scope.hotterColder = 'Getting colder...';
+        } else {
+          $scope.hotterColder = 'Getting warmer...';
         };
-        if (Math.abs(snapshot.val().distance - $scope.distanceToTarget) > 0.005) {
-          if ( snapshot.val().distance < $scope.distanceToTarget ) {
-            $scope.hotterColder = 'Getting colder...';
-          } else {
-            $scope.hotterColder = 'Getting warmer...';
-          };
 
-          userData.update( {distance: $scope.distanceToTarget} );
-        }
+        userData.update( {distance: $scope.distanceToTarget} );
+
       });
 
       checkpointData.update( dataChanges($scope.distanceToTarget) );
