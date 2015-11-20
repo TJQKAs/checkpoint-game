6765a3575dadfe77447ac788dfa6c9d4defcb6f9
TJQKAS <A26052013@GMAIL.COM>
2015-11-20 00:19:33 +0000

    update

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/app/index.html');
 });
 
+var express = require('express'),
+app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
 var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
